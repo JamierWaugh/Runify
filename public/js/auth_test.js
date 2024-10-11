58c69e99de18b1ee97ccf88c79cb89c787765014
Jamie Waugh <jamierwaugh@gmail.com>
2024-10-11 16:51:03 +0100

    Strava outputs streams and Lastfm outputs session token Added a few more api calls to the strava javascript - could do with tidying up

--- public/js/auth_test.js
+++ public/js/auth_test.js
@@ -1,7 +1,11 @@
-function auth(){
-    const body = new URLSearchParams({
-        api_key: "42e73efade36ebbeaa804a3bbdcc3d08",
+import {lkey} from "../../keys.js";
+
+export function auth(){
+    const body = new URLSearchParams({ /*Used to direct to callback and load token */
+        api_key: lkey.api_key,
         cb: "http://127.0.0.1:5500/Projects/Runify/views/callback.html"
     }).toString()
     return window.location.href = "https://www.last.fm/api/auth?" + body
 }; 
+
+window.auth = auth;
--- public/js/strava_api.js
+++ public/js/strava_api.js
@@ -1,17 +1,20 @@
-import key from "./keys.js";
-import {convertUnix} from "./public/js/unix_converter.js";
+import key from "../../keys.js";
+import {convertUnix} from "./unix_converter.js";
 /* Calls in keys from file */
 /* Calls in convertUnix function */
 const auth_link = "https://www.strava.com/oauth/token"
 
 /*Creates get request for all activity data from user*/
-function getActivities(res){
-    console.log(res)
-    console.log(res.access_token)
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+function getActivities(d){
+    const accessT = d.access_token
+    console.log(accessT, "access") /*Reads it just fine? Still pushing errors on line 9*/
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessT}`
     fetch(activities_link)
         .then((res) =>res.json())
             .then((res) => filterActivities(res)) /*Need to fix return on filterActivites*/
+    activityData(accessT)
+    getStreams(accessT)
+            
 
     
 }
@@ -30,8 +33,8 @@ function getAccess(){
             refresh_token: key.refreshToken,
             grant_type: "refresh_token",
         })
-    }).then(res => res.json())
-        .then((res) => getActivities(res))
+    }).then((res) => res.json())
+        .then(data => getActivities(data))
 }
 
 /*Filters activities to only include runs - this could be broadend as app is developed */
@@ -53,9 +56,60 @@ function filterActivities(res){
     return res
 }
 
+function activityData(accessT){
+    fetch("https://www.strava.com/api/v3/activities/12415852967",{ /*Change the id to have whichever the user has submitted */
+        method:"GET",
+        headers:{
+            "Authorization": `Bearer ${accessT}`
+        }
+    })
+    .then((response) => response.json())
+        .then((data) => getSplits(data))
+
+}
+
+function getStreams(accessT){
+    let streams = "time,velocity_smooth,altitude,grade_smooth"
+    fetch(`https://www.strava.com/api/v3/activities/12415852967/streams?keys=${streams}&key_by_type=true`, {
+        method:"GET",
+        headers:{
+            "Authorization": `Bearer ${accessT}`
+        },
+    })
+    .then((response) => response.json())
+        .then((data) => console.log(data))
+
+}
+
+function getSplits(data){ /*Converting split data into time stamp for music stats */
+    console.log(data)
+    const splits = data.splits_metric /*Relying on splits data per km */
+    console.log(splits)
+    let highest_GAP = {GAP: 0, pointer: -1,time:0}
+    for (let i = 0; i < splits.length; i++){
+        if (splits[i].average_grade_adjusted_speed > highest_GAP.GAP){
+            highest_GAP.pointer = i
+            highest_GAP.GAP = splits[i].average_grade_adjusted_speed
+        }
+
+    }
+    if (highest_GAP.pointer == 0){
+        highest_GAP.time = splits[0].elapsed_time
+    }
+    else{
+        for (let t = 0; t < highest_GAP.pointer; t++){
+            highest_GAP.time += splits[t].elapsed_time
+        }
+    }
+    
+    console.log(highest_GAP.time)
+    console.log(highest_GAP.GAP)
+}
+
 getAccess()
 
 getActivities()
+
 
 
 
